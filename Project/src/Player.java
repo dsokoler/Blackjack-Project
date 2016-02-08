c2501e779cb7f02d83263dc4854ff167266efb97
Chazuke <smcclel@purdue.edu>
2016-02-08 16:38:35 -0500

    Readding Player.java cause i think i deleted it

---
+++ Project/src/Player.java
@@ -0,0 +1,17 @@
+public class Player {
+	int numChips;
+	Card one;
+	Card two;
+	
+	public int getNumChips() { return numChips; }
+	
+	public void setNumChips(int set) { numChips = set; }
+	
+	public Card getCardOne() { return one; }
+	
+	public void setCardOne(Card set) { one = set; }
+	
+	public Card getCardTwo() { return two; }
+	
+	public void setCardTwo(Card set) { two = set; }
+}
